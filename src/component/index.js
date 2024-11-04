f86e2f602d06409ef7ec0224c52a50681a7225c3
Abdul Rehman <ar.saifi.133@gmail.com>
2024-11-04 19:18:38 +0530

    footer error fixed

--- src/component/index.js
+++ src/component/index.js
@@ -1,5 +1,5 @@
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
